f1984ca0fd9c7e1a9da5e2619e46299416dac7ab
foolbread <981848153@qq.com>
2017-08-10 20:50:46 +0800

    +服务基础框架

--- server/server.go
+++ server/server.go
@@ -7,6 +7,10 @@ package server
 
 import (
 	"github.com/foolbread/fbcommon/golog"
+	"github.com/foolbread/fbftp/config"
+
+	"net"
+	"fmt"
 )
 
 func InitServer(){
@@ -14,5 +18,51 @@ func InitServer(){
 }
 
 type fbFTPSever struct {
-	
+	server_port int
+	pasv_min_port int
+	pasv_max_port int
+}
+
+func newFBFTPServer()*fbFTPSever{
+	r := new(fbFTPSever)
+	r.server_port = config.GetConfig().GetPort()
+	r.pasv_min_port = config.GetConfig().GetPasvMinPort()
+	r.pasv_max_port = config.GetConfig().GetPasvMaxPort()
+
+	return r
+}
+
+func (s *fbFTPSever)run(){
+
+}
+
+func (s *fbFTPSever)startServerListen(){
+	serverAddr,err := net.ResolveTCPAddr("tcp",fmt.Sprintf(":%d",s.server_port))
+	if err != nil{
+		golog.Critical(err)
+	}
+
+	li,err := net.ListenTCP("tcp",serverAddr)
+	if err != nil{
+		golog.Critical(err)
+	}
+
+	for{
+		li.Accept()
+	}
+}
+
+func (s *fbFTPSever)startPasvListen(){
+	for i:=s.pasv_min_port; i<=s.pasv_max_port; i++{
+		pasvAddr,err := net.ResolveTCPAddr("tcp",fmt.Sprintf(":%d",i))
+		if err != nil{
+			golog.Critical(err)
+		}
+
+		li,err := net.ListenTCP("tcp",pasvAddr)
+
+		go func(){
+			li.Accept()
+		}()
+	}
 }
