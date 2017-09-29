b8e282539fdf6d7c142b0c7b09a153c316992410
foolbread <981848153@qq.com>
2017-09-29 14:52:23 +0800

    +新增支持list命令

--- protocol/command_List.go
+++ protocol/command_List.go
@@ -7,6 +7,12 @@ package protocol
 
 import (
 	"github.com/foolbread/fbftp/session"
+	"path"
+	"fmt"
+)
+
+const(
+	list_fmt = "%s 1 %s %s %d %s %s\r\n"//mod owner group size time name
 )
 
 type commandList struct {
@@ -26,5 +32,28 @@ func (p *commandList)RequireParam()bool{
 }
 
 func (p *commandList)Execute(sess *session.FTPSession, arg string)error{
-	return nil
+	if !sess.UserAcl.AllowRead(){
+		return sess.CtrlCon.WriteMsg(FTP_NOPERM,"Permission denied.")
+	}
+
+	arg = path.Clean(arg)
+
+	infos,err := sess.Storage.ListFile(path.Join(sess.UserAcl.GetWorkPath(),arg))
+	if err !=nil{
+		return err
+	}
+
+	sess.CtrlCon.WriteMsg(FTP_DATACONN,"Here comes the directory listing.")
+	if sess.DataCon == nil{
+		return sess.CtrlCon.WriteMsg(FTP_BADSENDNET,"Failure writing network stream.")
+	}
+
+	for _,v := range infos{
+		sess.DataCon.Write([]byte(fmt.Sprintf(list_fmt,v.Mode.String(),
+			v.Owner,v.Group,v.Size,v.ModTime.Format("Jan 2 15:04"),v.Name)))
+	}
+	sess.DataCon.Close()
+	sess.DataCon = nil
+
+	return 	sess.CtrlCon.WriteMsg(FTP_TRANSFEROK,"Directory send OK.")
 }
