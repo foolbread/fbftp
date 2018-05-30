e6e4da8e0d0fc637d5ac4b60a0f4a3aab5135679
foolbread <981848153@qq.com>
2018-05-30 16:34:26 +0800

    *完善help命令

--- protocol/command_help.go
+++ protocol/command_help.go
@@ -28,6 +28,8 @@ func (p *commandHelp)RequireParam()bool{
 func (p *commandHelp)Execute(sess *session.FTPSession, arg string)error{
 	sess.CtrlCon.WriteHyphen(FTP_HELP,"The following commands are recognized.")
 	sess.CtrlCon.WriteRaw(" FEAT HELP LIST PASS PASV PWD QUIT SYST USER\r\n")
+	sess.CtrlCon.WriteRaw(" ABOR CDUP CWD DELE  MKD NOOP PORT RETR RMD\r\n")
+	sess.CtrlCon.WriteRaw(" RNFR RNTO SIZE STAT STOR TYPE\r\n")
 
 	return sess.CtrlCon.WriteMsg(FTP_HELP,"help ok.")
 }
